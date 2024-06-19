9b49df6e7c7d8253b08551a7d59728af27ea24ad
于邦飞 <528154320@qq.com>
2024-06-19 10:18:59 +0800

    add godotenv

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello blog")
+}
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("env load failed: ", err)
+	}
 }
