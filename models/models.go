54ab35d47276fb214f615b14c62ab6832ff5000f
于邦飞 <528154320@qq.com>
2024-06-19 12:29:23 +0800

    add models

---
+++ models/models.go
@@ -0,0 +1,15 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uint `gorm:"primaryKey"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+	Name      string
+}
